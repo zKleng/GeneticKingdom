730cd73e22d2727177227abbb7484f00a385a1d1
DAniels0100 <se.alchanto@gmail.com>
2025-05-09 19:25:11 -0600

    CMakeLists.txt implementation 2

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -141,4 +141,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/GeneticKingdom.dir/DependInfo.cmake"
+  "CMakeFiles/run.dir/DependInfo.cmake"
   )
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,5 +1,3 @@
-# Install script for directory: /Users/sergiodanielalvarezchanto/desktop/GeneticKingdom
-
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
   set(CMAKE_INSTALL_PREFIX "/usr/local")
